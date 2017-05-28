03a30c935bf27b899dccd19b41c20c18cbf31040
Mark Hoggatt <markho@poulenc.hoggatt.me.uk>
2017-05-28 09:52:10 +0100

    Initial commit.

--- SqlEdit/ViewController.swift
+++ SqlEdit/ViewController.swift
@@ -8,20 +8,20 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-	override func viewDidLoad() {
+class ViewController: NSViewController
+{
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
 	}
 
-	override var representedObject: Any? {
-		didSet {
+	override var representedObject: Any?
+	{
+		didSet
+		{
 		// Update the view, if already loaded.
 		}
 	}
-
-
 }
-

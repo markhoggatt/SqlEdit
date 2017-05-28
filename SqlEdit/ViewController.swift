b319f0f2ed5963f421d094a3a5dff586c966a284
Mark Hoggatt <markho@poulenc.hoggatt.me.uk>
2017-05-28 09:44:17 +0100

    Initial Commit

---
+++ SqlEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SqlEdit
+//
+//  Created by Mark Hoggatt on 28/05/2017.
+//  Copyright © 2017 Code Europa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
